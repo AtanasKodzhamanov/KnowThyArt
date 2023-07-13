60feb2be664a742aac03109f89134f4e96d78b8a
Atanas <kodzhamanov@gmail.com>
2023-07-13 17:38:24 +0100

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ import os
 import dj_database_url
 
 
-DATABASE_URL = os.environ['DATABASE_URL_KTA']
+DATABASE_URL = os.environ['DATABASE_URL']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
