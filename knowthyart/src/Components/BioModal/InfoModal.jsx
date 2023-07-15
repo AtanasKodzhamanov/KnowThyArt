a90efaef4413e70cf6fd3dd6dd0034e5e48c5e85
Atanas <kodzhamanov@gmail.com>
2023-07-15 16:08:09 +0100

    fix animation bug

--- knowthyart/src/Components/BioModal/InfoModal.jsx
+++ knowthyart/src/Components/BioModal/InfoModal.jsx
@@ -2,15 +2,18 @@ import styles from './InfoModal.module.css';
 import React, { useState } from 'react';
 
 const InfoModal = ({ correctAnswer, artist, closeModal, bioModal, nextArtist, answerProvided }) => {
-    const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState(0);
     let storySections = artist.story.split("<SECTION>");
+
     const imageStyle = {
         height: "100vh",
         width: "50%",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         position: "absolute",
-        zIndex: -1
+        zIndex: -1,
+        opacity: answerProvided ? 1 : 0, // Update opacity based on answerProvided
+        transition: "opacity 0.5s ease-in-out", // Add a transition effect
     };
 
     const leftImageStyle = {
@@ -24,6 +27,7 @@ const InfoModal = ({ correctAnswer, artist, closeModal, bioModal, nextArtist, an
         backgroundImage: `url(${artist.img_2})`,
         right: 0
     };
+
     const shaderStyle = {
         height: "100vh",
         width: "100%",
@@ -31,7 +35,8 @@ const InfoModal = ({ correctAnswer, artist, closeModal, bioModal, nextArtist, an
         position: "absolute",
         zIndex: -1
     };
-    return(
+
+    return (
         <>
             <div style={leftImageStyle}></div>
             <div style={rightImageStyle}></div>
@@ -46,40 +51,51 @@ const InfoModal = ({ correctAnswer, artist, closeModal, bioModal, nextArtist, an
                     </div>
                 </div>
                 <div className={styles.buttonsRow}>
-                    { index == 0 && 
-                    <div className={styles.userMessage}>
-                        <div className={styles.successRate}>
-                            {correctAnswer == true ? <h3>You have a keen eye!</h3>
-                            : <h3>Wrong, this is the style of {artist.name}!</h3>}
+                    {index === 0 && (
+                        <div className={styles.userMessage}>
+                            <div className={styles.successRate}>
+                                {correctAnswer ? (
+                                    <h3>You have a keen eye!</h3>
+                                ) : (
+                                    <h3>Wrong, this is the style of {artist.name}!</h3>
+                                )}
+                            </div>
+                            <div>
+                                <h3>
+                                    Success Rate-
+                                    {Math.round((artist.correct_answer / (artist.correct_answer + artist.incorrect_answer)) * 100)}%
+                                </h3>
+                            </div>
                         </div>
-                        <div>
-                            <h3>
-                            Success Rate-    
-                                {  
-                                    Math.round(
-                                    (artist.correct_answer / (artist.correct_answer + artist.incorrect_answer)) * 100
-                                    ) 
-                                }%
-                            </h3>
-                        </div> 
-                    </div> }
+                    )}
                     <div className={styles.buttonGroup}>
                         <div>
-                        {index < storySections.length - 1 && 
-                            <button 
-                                className={styles.nextButton} 
-                                onClick={() => setIndex(prev => prev+1)}>
+                            {index < storySections.length - 1 && (
+                                <button
+                                    className={styles.nextButton}
+                                    onClick={() => setIndex(prev => prev + 1)}
+                                >
                                     <h3>Read More</h3>
-                            </button>}
+                                </button>
+                            )}
                         </div>
                         <div>
-                            <button className={styles.nextButton} onClick={()=>{nextArtist(); closeModal()}}><h3>Next Artist</h3></button>
+                            <button
+                                className={styles.nextButton}
+                                onClick={() => {
+                                    nextArtist();
+                                    closeModal();
+                                    setIndex(0);
+                                }}
+                            >
+                                <h3>Next Artist</h3>
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
         </>
-    )
-}   
+    );
+};
 
 export default InfoModal;
--- knowthyart/src/Components/Gallery/Gallery.jsx
+++ knowthyart/src/Components/Gallery/Gallery.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Gallery.module.css';
 
-const Gallery = ({artist, answerProvided}) => {
+const Gallery = ({ artist, answerProvided }) => {
+    const [resetAnimation, setResetAnimation] = useState(false);
+
+    useEffect(() => {
+        setResetAnimation(true);
+        const timeout = setTimeout(() => {
+            setResetAnimation(false);
+        }, 0);
+
+        return () => clearTimeout(timeout);
+    }, [artist]);
 
     return (
-        <>        
-        { artist ? 
-            <div className={styles.galleryContainer}>
-                <div className={styles.painting} >
-                    <img src={artist.img_1}/>
+        <>
+            {artist ? (
+                <div key={artist.name} className={`${styles.galleryContainer} ${resetAnimation ? styles.resetAnimation : ''}`}>
+                    <div className={styles.painting}>
+                        <img src={artist.img_1} alt="Painting 1" />
+                    </div>
+                    <div className={styles.painting}>
+                        <img src={artist.img_2} alt="Painting 2" />
+                    </div>
+                    <div className={styles.painting}>
+                        <img src={artist.img_3} alt="Painting 3" />
+                    </div>
                 </div>
-                <div className={styles.painting}>
-                    <img src={artist.img_2}  />
-                </div>
-                <div className={styles.painting}>
-                    <img src={artist.img_3} />
-                </div>
-            </div>
-        : null
-        }
-
+            ) : null}
         </>
-    )
-}
+    );
+};
 
 export default Gallery;
