3ef6144b7526b7a5e0b87e06984e24584d1afeae
Atanas <kodzhamanov@gmail.com>
2023-07-20 15:27:01 +0100

    Secret key and shading

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,9 +27,9 @@ CSRF_TRUSTED_ORIGINS = ['https://knowthy.art',
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_fg!7r$i=-w!49jgivh3eal4aj+anl0k-=dyvgk*k$+y*yz=w-'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY_KTA']
+'django-insecure-_fg!7r$i=-w!49jgivh3eal4aj+anl0k-=dyvgk*k$+y*yz=w-'
 
 # SECRET_KEY = os.environ["SECRET_KEY_KTA"]
 # RECAPTCHA_PUBLIC_KEY = os.environ["SITE_KEY_KTA"]
