8f6fae1f6473b8cfbbeb834ccb587374494e98a3
Atanas <kodzhamanov@gmail.com>
2024-01-18 02:14:10 +0200

    Update DATABASE_URL environment variable

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ import os
 import dj_database_url
 
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL_KNOW_THY_ART']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
