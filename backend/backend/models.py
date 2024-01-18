1d1d800811e0cedf6b2582de065bcb4c75a979be
Atanas <kodzhamanov@gmail.com>
2024-01-18 02:34:59 +0200

    Update models.py

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from django.db import models
-
-
 class Artist(models.Model):
     name = models.CharField(max_length=200, blank=True, default="")
     img_1 = models.URLField(max_length=200, blank=True, default="")
